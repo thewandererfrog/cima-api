6f59bf8e6122b5e8ce01b80c07e3bc9ab674b3a5
Diogo Afonso Barroso <diogo.barroso@protonmail.com>
2020-02-17 06:37:43 +0100

    updates db connection

--- database/db_connect.py
+++ database/db_connect.py
@@ -2,17 +2,11 @@ from playhouse.postgres_ext import PostgresqlExtDatabase
 import os
 import time
 
-global count
-count= 0
 # Connect to a Postgres database.
 def getDB():
-    global count    
-    print(count)
-    
-    count = count+1
     try:
         return PostgresqlExtDatabase(
-            'cima', 
+            db=os.getenv('DATABASE_DB'), 
             user=os.getenv('DATABASE_USER'), 
             password=os.getenv('DATABASE_PASS'),
             host=os.getenv('DATABASE_URL'), 
